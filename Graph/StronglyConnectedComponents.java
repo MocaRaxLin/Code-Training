7e50a8f23d0c309faecc9c5a286398199f85fefe
JustinLin <tony199462@gmail.com>
2018-07-24 13:37:10 +0800

    Add SCC Tarjans

--- Graph/StronglyConnectedComponents.java
+++ Graph/StronglyConnectedComponents.java
@@ -2,9 +2,11 @@ package Graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import Util.Parser;
 import Util.Show;
@@ -32,9 +34,92 @@ public class StronglyConnectedComponents {
 			List<List<Integer>> ans1 = sol.Kosaraju(V, E);
 			System.out.print("Kosaraju's SCC: ");
 			show.showListListInt(ans1);
+			
+			List<List<Integer>> ans2 = sol.Tarjans(V, E);
+			System.out.print("Tarjans' SCC: ");
+			show.showListListInt(ans2);
 		}
 		
 	}
+
+	private List<List<Integer>> Tarjans(int V, int[][] E) {
+		// --> time = O(V+E), one time DFS.
+		
+		// source: 
+		// https://www.youtube.com/watch?v=TyWtx7q2D7Y&feature=youtu.be
+		
+		// Build graph of adjacency lists
+		List<List<Integer>> adj = new ArrayList<List<Integer>>();
+		for(int i = 0; i < V; i++) adj.add(new LinkedList<Integer>());
+		for(int[] e: E) adj.get(e[0]).add(e[1]);
+		
+		// set and update node low link values
+		low = new int[V];
+		ids = new int[V];
+		Arrays.fill(ids, -1); // all nodes are not visited
+		newId = 0;
+		
+		stack = new int[V+1];
+		stack[0] = 1;
+		onStack = new boolean[V];
+		
+		// one time DFS
+		for(int i = 0; i < V; i++) {
+			if(ids[i] != -1) continue;
+			DFSLow(adj, i);
+		}
+		//System.out.println("low value: " + Arrays.toString(low));
+		
+		// Collect SCC
+		List<List<Integer>> SCC = new ArrayList<List<Integer>>();
+		int SCCCount = 0;
+		Map<Integer, Integer> lowToList = new HashMap<Integer, Integer>();
+		for(int i = 0; i < low.length; i++) {
+			if(!lowToList.containsKey(low[i])) {
+				lowToList.put(low[i], SCCCount++);
+				SCC.add(new LinkedList<Integer>());
+			}
+		}
+		for(int i = 0; i < low.length; i++) {
+			int idx = lowToList.get(low[i]);
+			SCC.get(idx).add(i);
+		}
+		return SCC;
+	}
+	
+	int[] low;
+	int[] ids;
+	int newId;
+	int[] stack;
+	boolean[] onStack;
+	private void DFSLow(List<List<Integer>> adj, int root) {
+		//System.out.println(root);
+		stack[stack[0]++] = root;
+		onStack[root] = true;
+		ids[root] = low[root] = newId++;
+		
+		// Visit all neighbors and minimize low-link on callback
+		Iterator<Integer> it = adj.get(root).iterator();
+		while(it.hasNext()) {
+			int next = it.next();
+			if(ids[next] == -1) DFSLow(adj, next);
+			if(onStack[next]) low[root] = Math.min(low[root], low[next]);
+		}
+		
+		// After having visited all the neighbors of root
+		// If we are at the start of SCC,
+		// empty the seen stack until we are back to the start of SCC.
+		if(ids[root] == low[root]) {
+			while(stack[0] > 1 && stack[stack[0]-1] != root) {
+				int node = stack[--stack[0]];
+				onStack[node] = false;
+				low[node] = ids[root];
+			}
+			stack[0]--; // pop the root
+			onStack[root] = false;
+		}
+	}
+	
 
 	public List<List<Integer>> Kosaraju(int V, int[][] E) {
 		// --> time = O(V+E) in linear time.
